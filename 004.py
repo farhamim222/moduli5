dcc1e3d50fbb8ef098a384481b922117abf28e19
farha <Farha.Mim@metropolia.fi>
2024-09-03 23:54:16 +0300

    done

---
+++ 004.py
@@ -0,0 +1,8 @@
+kaupungit = ['Helsinki', 'Turku', 'Tampere' 'Oulu' 'Jyväskylä']
+
+for i in range(5):
+    kaupunki = input("Anna kaupungin nimi:Helsinki ")
+    kaupungit.append('Helsinki')
+
+for kaupunki in kaupungit:
+    print(kaupunki)
